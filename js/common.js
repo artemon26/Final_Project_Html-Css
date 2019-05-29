6dc71fde21149360ae2f55eb8b6d01321f07b1e1
artemon26 <artem.burak95@gmail.com>
2019-05-29 14:42:49 +0300

    Html-Css

---
+++ js/common.js
@@ -0,0 +1,22 @@
+// Scrloll
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({scrollTop: 0}, 600);
+        return false;
+    });
+
+});
+
+// Trigger
+function trigger() {
+    $('.none2').trigger('click');
+}
